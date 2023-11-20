6ac3b3be5c5e3e94b2830222efea701662db0da7
tommyprHub <tomasprimo.alu@iespacomolla.es>
2023-11-20 15:23:11 +0100

    commit

--- app/src/main/java/com/example/reciclerviewexample/adapter/CocheAdapter.kt
+++ app/src/main/java/com/example/reciclerviewexample/adapter/CocheAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.reciclerviewexample.adapter
 
-class CocheAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reciclerviewexample.Coche
+import com.example.reciclerviewexample.R
+
+class CocheAdapter(private val cocheList:List<Coche>) : RecyclerView.Adapter<CocheViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocheViewHolder {
+        //aquí le paso el item (el layout que va a modificar)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return CocheViewHolder(layoutInflater.inflate(R.layout.item_coche, parent, false))
+    }
+
+    override fun getItemCount(): Int = cocheList.size
+
+    override fun onBindViewHolder(holder: CocheViewHolder, position: Int) {
+        val item = cocheList[position]
+        holder.render(item)
+    }
 }
--- app/src/main/java/com/example/reciclerviewexample/adapter/CocheViewHolder.kt
+++ app/src/main/java/com/example/reciclerviewexample/adapter/CocheViewHolder.kt
@@ -1,4 +1,21 @@
 package com.example.reciclerviewexample.adapter
 
-class CocheViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.reciclerviewexample.Coche
+import com.example.reciclerviewexample.R
+
+class CocheViewHolder(view:View):RecyclerView.ViewHolder(view){
+
+    val coche = view.findViewById<TextView>(R.id.tvCoche)
+    val marca = view.findViewById<TextView>(R.id.tvMarcaCoche)
+    val photo = view.findViewById<ImageView>(R.id.ivCoche)
+    fun render(cocheModel:Coche){
+        coche.text = cocheModel.coche
+        marca.text = cocheModel.marca
+
+    }
 }
